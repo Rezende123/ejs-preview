58c37d3560347d34d5b9fe810c3c6e3d16ffb1a9
Felipe Rezende <feliperezende.srz@gmail.com>
2020-06-22 18:53:27 -0300

    Organização do layout inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/template/menu/menu.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { ContentComponent } from './components/template/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
